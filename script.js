2833a588fd3524320643ee684d9b6acb289e8497
soso <164677008+sopynya@users.noreply.github.com>
2025-10-04 14:35:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function hamburguerDisplay() {
 
   if (nav.style.display === "block") {
     nav.style.display = "none";
-    hamb.src = "/images/icon-hamburger.svg";
+    hamb.src = "images/icon-hamburger.svg";
   } else {
     nav.style.display = "block";
-    hamb.src = "/images/icon-close.svg";
+    hamb.src = "images/icon-close.svg";
   }
 }
